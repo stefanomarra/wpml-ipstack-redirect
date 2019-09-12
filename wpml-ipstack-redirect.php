4184c30e5136b58d0ee21082d3921b6366978f03
Stefano Marra <stefano.marra1987@gmail.com>
2019-09-12 15:28:04 +0200

    Added force redirect feature

--- wpml-ipstack-redirect.php
+++ wpml-ipstack-redirect.php
@@ -75,6 +75,14 @@ class WPML_IPStack_Redirect
         }
     }
 
+    /**
+     * https://wordpress.stackexchange.com/questions/12863/check-if-wp-login-is-current-page
+     */
+    function is_wp_login() {
+		$ABSPATH_MY = str_replace(array('\\','/'), DIRECTORY_SEPARATOR, ABSPATH);
+		return ((in_array($ABSPATH_MY.'wp-login.php', get_included_files()) || in_array($ABSPATH_MY.'wp-register.php', get_included_files()) ) || (isset($_GLOBALS['pagenow']) && $GLOBALS['pagenow'] === 'wp-login.php') || $_SERVER['PHP_SELF']== '/wp-login.php');
+    }
+
     function redirect_user( $location ){
 		header("Location: $location");
 		exit();
@@ -82,6 +90,22 @@ class WPML_IPStack_Redirect
 
 	function get_api_key() {
 		return apply_filters( 'wpml_ipstack_redirect_get_api_key', get_option('wpml_ipstack_redirect_api_key') );
+	}
+
+	function force_redirect() {
+		if ( $this->is_wp_login() ) {
+			return apply_filters( 'wpml_ipstack_redirect_force_redirect', false );
+		}
+
+		if ( is_admin() ) {
+			return apply_filters( 'wpml_ipstack_redirect_force_redirect', false );
+		}
+
+		if ( current_user_can('editor') || current_user_can('administrator') ) {
+
+		}
+
+		return apply_filters( 'wpml_ipstack_redirect_force_redirect', true );
 	}
 
 	function get_client_ip() {
@@ -124,6 +148,10 @@ class WPML_IPStack_Redirect
 	function redirect_ip_country() {
 		global $wpdb, $post, $sitepress, $sitepress_settings;
 
+		if ( !$this->get_api_key() ) {
+			return;
+		}
+
 		$args['skip_missing'] = intval($sitepress_settings['automatic_redirect'] == 1);
 		$languages = apply_filters( 'wpml_active_languages', null, $args );
 		$language_urls = array();
@@ -132,15 +160,24 @@ class WPML_IPStack_Redirect
 		}
 
 		// echo '<pre>';
+		// var_dump(get_site_url());
 		// var_dump($language_urls);
+		// var_dump($this->get_country_code_by_ip());
+		// var_dump($sitepress->get_default_language());
 		// die();
 
-		if ( $this->get_api_key() && !isset($_COOKIE['icl_ip_to_country_check']) ) {
+		/**
+		 * No Cookies are set yet
+		 */
+		if ( !isset($_COOKIE['icl_ip_to_country_check']) || !isset($_COOKIE['icl_ip_to_country_lang']) ) {
 
 			// Grab the language code from ip e.g. es
 			$lang_code_from_ip = $this->get_language_code_by_ip();
 			if ( !$lang_code_from_ip ) {
 				$lang_code_from_ip = $this->get_country_code_by_ip();
+			}
+			if ( !$lang_code_from_ip ) {
+				$lang_code_from_ip = $sitepress->get_default_language();
 			}
 
 			// Get the current post, page language code
@@ -154,6 +191,7 @@ class WPML_IPStack_Redirect
 
 			// Now lets set a browser cookie and do the redirect
 			setcookie('icl_ip_to_country_check','checked', time()+($sitepress_settings['remember_language']*HOUR_IN_SECONDS), COOKIEPATH, COOKIE_DOMAIN);
+			setcookie('icl_ip_to_country_lang', $lang_code_from_ip, time()+($sitepress_settings['remember_language']*HOUR_IN_SECONDS), COOKIEPATH, COOKIE_DOMAIN);
 
 			// If language is set, redirect to that url
 			if ( isset($language_urls[$lang_code_from_ip]) ) {
@@ -163,6 +201,32 @@ class WPML_IPStack_Redirect
 			// Language not found, redirect to default language
 			else {
 				wp_safe_redirect( $language_urls[$sitepress->get_default_language()] );
+			}
+		}
+
+		/**
+		 * Force redirect the user to the right post.
+		 * Make sure to redirect only if user is currently viewing the post in a different language then the one store in the cookie
+		 */
+		else if ( $this->force_redirect() && isset($_COOKIE['icl_ip_to_country_lang']) && !empty($_COOKIE['icl_ip_to_country_lang']) ) {
+			$lang_code_from_ip = $_COOKIE['icl_ip_to_country_lang'];
+			$post_language_code = ICL_LANGUAGE_CODE;
+
+			$current_post_url = isset($language_urls[$post_language_code])?$language_urls[$post_language_code]:false;
+			$user_lang_post_url = isset($language_urls[$lang_code_from_ip])?$language_urls[$lang_code_from_ip]:false;
+
+			// echo '<pre>';
+			// var_dump($lang_code_from_ip);
+			// var_dump($post_language_code);
+			// var_dump($user_lang_post_url);
+			// var_dump($language_urls);
+			// die();
+
+			/**
+			 * If the current post language is different then the user language then redirect
+			 */
+			if ( $lang_code_from_ip != $post_language_code && $current_post_url != $user_lang_post_url ) {
+				wp_safe_redirect( $user_lang_post_url );
 			}
 		}
 	}
